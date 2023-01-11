44b517624bb91e7d94422d81904c6d0cb7861a18
efim <dedefim56@gmail.com>
2023-01-11 19:19:17 +0500

    lesson_4_2 migrate

--- Рабочий стол/djangoBasics/Braniacl MS/config/settings.py
+++ Рабочий стол/djangoBasics/Braniacl MS/config/settings.py
@@ -82,6 +82,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
