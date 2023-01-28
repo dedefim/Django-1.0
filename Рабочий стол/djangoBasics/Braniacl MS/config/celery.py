bb373829371e64e7528dfc883a09ed4e028918bb
dedefim <100861058+dedefim@users.noreply.github.com>
2023-01-28 19:26:44 +0500

    Revert "lesson_8"

--- Рабочий стол/djangoBasics/Braniacl MS/config/celery.py
+++ Рабочий стол/djangoBasics/Braniacl MS/config/celery.py
@@ -1,8 +1,6 @@
 import os
-from django.conf import settings
 from celery import Celery
-if settings.DEBUG:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 celery_app = Celery("braniac")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
